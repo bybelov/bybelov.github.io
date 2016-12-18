6f3543ac4840ea7e779e93b52c06f6b698586a36
Igor <bybelov@gmail.com>
2016-12-18 22:06:57 +0300

    Update script.js

--- js/1/script.js
+++ js/1/script.js
@@ -1,1 +1,16 @@
+var myName = prompt('Как ваше Имя?');
+var mySurname = prompt('Как ваша Фамилия?')
+var myBirthYear = prompt('Введите ваш год рождения');
 
+var currentYear = 2016;
+var age = currentYear - myBirthYear;
+
+if(age < 20){
+    alert('Привет, ' + myName + ' ' + mySurname + '!');
+}
+else if(age >= 20 && age < 40){
+    alert('Добрый день, ' + myName + ' ' + mySurname);
+}
+else{
+    alert('Здравствуйте, ' + myName + ' ' + mySurname);
+}
