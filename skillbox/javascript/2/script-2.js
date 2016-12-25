ba6ec8b5c6c016954f4226fe43cdba964bd8f082
Igor <bybelov@gmail.com>
2016-12-26 01:31:25 +0300

    update theme2

--- skillbox/javascript/2/script-2.js
+++ skillbox/javascript/2/script-2.js
@@ -1,3 +1,4 @@
+// Алгоритм определения високосного года
 function IsLeapYear(year) {
     if(year % 4 == 0) {
         if(year % 100 == 0) {
@@ -12,3 +13,33 @@ function IsLeapYear(year) {
     }
     return false;
 }
+
+var x, y;
+var startYear = +prompt('Введите начальный год'); // Запрашиваем год и преобразуем в число
+var endYear = +prompt('Введите конечный год');
+
+//Определяем, из двух введенных годов максимальный
+if(startYear > endYear){
+    x = endYear;
+    y = startYear;
+}
+else{
+    x = startYear;
+    y = endYear;
+}
+
+for(x; x <= y; x++){
+    if(IsLeapYear(x)){
+        console.log(x + '\n');
+    }
+}
+
+
+/*
+// Вариант программы для определения високосного года с 1900 по 2016
+for(x = 1900; x <= 2016; x++){
+    if(IsLeapYear(x)){
+        console.log(x + '\n');
+    }
+}
+*/
