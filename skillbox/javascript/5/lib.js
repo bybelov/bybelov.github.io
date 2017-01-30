3d8c2c7d7061836c8da36a9e9b1c5a9e4a40ba91
Igor <bybelov@gmail.com>
2017-01-30 22:10:00 +0300

    final theme 5

--- skillbox/javascript/5/lib.js
+++ skillbox/javascript/5/lib.js
@@ -2,19 +2,26 @@
 
 (function (){
 
-    function rand(min,max){
+    // функция для генерации случайного числа, в заданных интервалах
+    function getRandomNumber(min,max){
         return Math.floor(Math.random() * (max - 1)) + min;
     }
 
-    var random = rand(1,1000);
+    // Генерируем случайное число
+    var random = getRandomNumber(1,1000);
+    // Устанавливаем начальное значение для подсчета попыток угадывания числа
     var counter = 1;
 
     return window.start = function (){
 
         while(true){
+
             console.log('Попытка №' + counter);
+
             if(counter <= 10){
+
                 console.log(random);
+
                 var number = prompt('Угадай число, которое я загадал.');
                 if(number === null){
                     console.log('Выход из программы');
@@ -29,17 +36,18 @@
                     if(number == random){
 
                         alert('Правильно!');
-
                         var  startAgain = confirm('Хотите поиграть ещё раз?');
+
                         if(!startAgain){
                             alert('Спасибо за игру!');
                             break;
                         }
                         else{
-                            random = rand(1,1000);
+                            // Сбрасываем случаное число, и генерируем новое
+                            random = getRandomNumber(1,1000);
+                            // Сбрасываем счетчик попыток. Сбрасываем до 0, так как в конце цикла прибавляем 1 к счетчику.
                             counter = 0;
                         }
-
                     }
                     else if(number > random){
                         alert('Меньше!');
@@ -55,6 +63,7 @@
             else{
                 return alert('Попытки закончились');
             }
+            // Увеличиваем счетчик попыток на 1, если число не угадано
             counter++;
         }
 
