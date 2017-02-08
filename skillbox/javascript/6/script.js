f21c8411f91363d9eebd5d86ca9726163ca5b409
Igor <bybelov@gmail.com>
2017-02-08 20:38:37 +0300

    theme 6

--- skillbox/javascript/6/script.js
+++ skillbox/javascript/6/script.js
@@ -15,15 +15,19 @@
             users.push(user);
         }
         this.getAllUsers = function(){
-            var div = document.getElementById('user_list');
-            var ol = document.createElement('ol');
-            div.appendChild(ol);
+            // Проверяем, не пустой ли массив users пришел
+            if(users.length > 0){
+                var div = document.getElementById('user_list');
+                var ol = document.createElement('ol');
+                div.appendChild(ol);
+                // в цикле перебираю все объекты в массиве users
+                for (var i = 0; i < users.length; i++){
 
-            for (var i = 0; i < users.length; i++){
-                var li = document.createElement('li');
-                li.innerHTML = users[i].firstName + ' ' + users[i].lastName + ' - ' + users[i].regDate  + '\n';
-                ol.appendChild(li);
-                //console.log( users[i].firstName + ' ' + users[i].lastName + ' ' + users[i].regDate  + '\n' );
+                    var li = document.createElement('li');
+                    li.innerHTML = users[i].firstName + ' ' + users[i].lastName + ' - ' + users[i].regDate;
+                    ol.appendChild(li);
+                    //console.log( users[i].firstName + ' ' + users[i].lastName + ' ' + users[i].regDate);
+                }
             }
         }
     }
@@ -37,12 +41,17 @@
         var userList = new UserList();
         while(true){
             var user = prompt('Введите Имя и Фамилию');
+            // Если нажали ESQ
             if(user === null){
+                // Выводим список пользователей
                 userList.getAllUsers();
                 return;
             }
-            var user = new User(user);
-            userList.add(user);
+            // Если поле не пустое, то заводим нового пользователя
+            else if (user != ''){
+                var user = new User(user);
+                userList.add(user);
+            }
         }
     }
 
