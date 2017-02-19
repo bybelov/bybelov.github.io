247f8b7d678ae278567aef68380e5b9fdc023df4
Igor <bybelov@gmail.com>
2017-02-20 00:26:18 +0300

    up

--- skillbox/javascript/7/script.js
+++ skillbox/javascript/7/script.js
@@ -6,26 +6,42 @@
 // • дочерние конструкторы: автомобиль, самолёт, корабль, которые наследуют общие свойства и методы от родительского
 // • продемонстрируйте, как дочерние сущности могут переопределять родительские свойства и методы в соответствии со своим собственным поведением
 
-(function (){
-
-    function Vehicle(speed,){
-        this.way = way; // Способ передвижения
-        this.speed = speed; // Скорость
-        this.cost = cost; // Стоимость билета
-        this.time = time; // Время в пути
+//(function (){
+	
+	// Базовая функция конструктор
+    function Vehicle(distance,speed){
+        this._distance = distance; // Расстояние, которое нужно преодолеть
+		this._speed = speed; // Средняя скорость
+		var self = this;
+		this.getTimeWay = function(){ // Время в пути
+			return 'Время в пути = ' + self._distance / self._speed + ' ч.';
+		}
     }
-
-    function Car(){
-        this.body = body; // Тип кузова
-        this.drive = drive; // Тип привода
+    function Car(distance,speed,body){
+		// Обращаемся к базовой функции для наследования свойств и методов
+		Vehicle.apply(this, arguments);
+		// Добавляем свой метод, который позволяет изменить значение свойства из метода базовой
+		// функции, и вызываем метод базовой функции повторно с учетом внесенных изменений
+		this.changeSpeed = function(speed){
+			this._speed = speed;
+			return this.getTimeWay();
+		}
     }
-    function Plane(){
+    function Plane(wingspan,business){
+		Vehicle.apply(this, arguments);
         this.wingspan = wingspan; // Размах крыльев
         this.business = business; // Наличие бизнес класса
     }
-    function Ship(){
+    function Ship(displacement,team){
+		Vehicle.apply(this, arguments);
         this.displacement = displacement; // водоизмещение
         this.team = team; // Экипаж корабля
     }
+	
+	
+	var car = new Car(150, 600);
+	var plane = new Plane(850, 600);
+	var ship = new Ship(60, 600);
+	car.changeSpeed(80);	
 
-})();
+//})();
