66728771509675b4f66516b3300c9361576238f1
Igor <bybelov@gmail.com>
2017-02-26 23:47:45 +0300

    update

--- skillbox/javascript/7/script.js
+++ skillbox/javascript/7/script.js
@@ -6,42 +6,63 @@
 // • дочерние конструкторы: автомобиль, самолёт, корабль, которые наследуют общие свойства и методы от родительского
 // • продемонстрируйте, как дочерние сущности могут переопределять родительские свойства и методы в соответствии со своим собственным поведением
 
-//(function (){
-	
+(function (){
+
 	// Базовая функция конструктор
     function Vehicle(distance,speed){
-        this._distance = distance; // Расстояние, которое нужно преодолеть
-		this._speed = speed; // Средняя скорость
+        this.distance = distance; // Расстояние, которое нужно преодолеть
+		this.speed = speed; // Средняя скорость
 		var self = this;
 		this.getTimeWay = function(){ // Время в пути
-			return 'Время в пути = ' + self._distance / self._speed + ' ч.';
+			return 'Время в пути = ' + self.distance / self.speed + ' ч.';
 		}
     }
-    function Car(distance,speed,body){
+    function Car(distance,speed){
 		// Обращаемся к базовой функции для наследования свойств и методов
 		Vehicle.apply(this, arguments);
-		// Добавляем свой метод, который позволяет изменить значение свойства из метода базовой
-		// функции, и вызываем метод базовой функции повторно с учетом внесенных изменений
+		// Добавляем собственный метод для Car(), который позволяет изменить значение свойства из метода базовой функции
+		// и вызываем метод базовой функции повторно с учетом внесенных изменений
 		this.changeSpeed = function(speed){
-			this._speed = speed;
+			this.speed = speed;
 			return this.getTimeWay();
 		}
     }
-    function Plane(wingspan,business){
-		Vehicle.apply(this, arguments);
-        this.wingspan = wingspan; // Размах крыльев
-        this.business = business; // Наличие бизнес класса
-    }
-    function Ship(displacement,team){
-		Vehicle.apply(this, arguments);
-        this.displacement = displacement; // водоизмещение
-        this.team = team; // Экипаж корабля
-    }
-	
-	
-	var car = new Car(150, 600);
-	var plane = new Plane(850, 600);
-	var ship = new Ship(60, 600);
-	car.changeSpeed(80);	
+    // function Plane(distance,speed){
+	// 	Vehicle.apply(this, arguments);
+    // }
+    // function Ship(distance,speed){
+	// 	Vehicle.apply(this, arguments);
+    // }
 
-//})();
+	// var button = document.getElementsByClassName('vehicle');
+    // button.onclick = function(){
+	// 	for (var i = 0; i < this.length; i++){
+    //         var attr = this.getAttribute('id');
+    //         start(this.attr);
+    //     }
+	// 	return false;
+    // };
+
+	return window.start = function(vehicle){
+
+		if(vehicle == 'Машина'){
+			console.log(vehicle);
+		}else if(vehicle == 'Самолет'){
+			console.log(vehicle);
+		}else if(vehicle == 'Корабль'){
+			console.log(vehicle);
+		}
+
+		var distance = prompt('Введите расстояние в км до цели');
+		var speed = prompt('Введите среднюю скорость для ' + vehicle + '.');
+		var car = new Car(distance, speed);
+		alert(car.getTimeWay());
+		var changeSpeedCar = confirm('Хотите изменить скорость и пересчитать время?');
+		if(changeSpeedCar){
+			var speed = prompt('Введите среднюю скорость для ' + vehicle + '.');
+			alert(car.changeSpeed(speed));
+		}
+	}
+
+
+})();
