3ccf1a148586c48adf92794ccecaa8f1dc3de348
Igor <bybelov@gmail.com>
2017-03-05 20:28:06 +0300

    theme 7. task 1

--- skillbox/javascript/7/script.js
+++ skillbox/javascript/7/script.js
@@ -1,12 +1,5 @@
-'use strict';
-
-// Создайте иерархию сущностей (конструкторов, из которых можно создать экземпляры объектов):
-//
-// • базовый конструктор «Транспортное средство» с общими для всех транспортных средств свойствами и методами (на ваше усмотрение)
-// • дочерние конструкторы: автомобиль, самолёт, корабль, которые наследуют общие свойства и методы от родительского
-// • продемонстрируйте, как дочерние сущности могут переопределять родительские свойства и методы в соответствии со своим собственным поведением
-
 (function (){
+    'use strict';
 
 	// Базовая функция конструктор
     function Vehicle(distance,speed){
@@ -14,9 +7,17 @@
 		this.speed = speed; // Средняя скорость
 		var self = this;
 		this.getTimeWay = function(){ // Время в пути
-			return 'Время в пути = ' + self.distance / self.speed + ' ч.';
+            if(isNaN(self.distance) || self.distance === null || self.distance == ''){
+                return 'Расстояние не задано';
+            }
+            if(isNaN(self.speed) || self.speed === null || self.speed == ''){
+                return 'Скорость не задана';
+            }
+            var result = Math.round((self.distance / self.speed) * 100) / 100;
+			return 'Время в пути = ' + result + ' ч.';
 		}
     }
+
     function Car(distance,speed){
 		// Обращаемся к базовой функции для наследования свойств и методов
 		Vehicle.apply(this, arguments);
@@ -27,42 +28,59 @@
 			return this.getTimeWay();
 		}
     }
-    // function Plane(distance,speed){
-	// 	Vehicle.apply(this, arguments);
-    // }
-    // function Ship(distance,speed){
-	// 	Vehicle.apply(this, arguments);
-    // }
 
-	// var button = document.getElementsByClassName('vehicle');
-    // button.onclick = function(){
-	// 	for (var i = 0; i < this.length; i++){
-    //         var attr = this.getAttribute('id');
-    //         start(this.attr);
-    //     }
-	// 	return false;
-    // };
+    function Plane(distance){
+        Vehicle.apply(this, arguments);
+        // Устанавливаем своё свойство для конструктора Plane
+        this.speed = 850; // Средняя скорость пассажирского самолета
+    }
 
-	return window.start = function(vehicle){
+    function Ship(distance,speed){
+		Vehicle.apply(this, arguments);
+        this.speed = 20; // Средняя скорость пассажирского речного судна
+        this.getSpeed = function(speed){
+            alert('Средняя скорость нашего корабля ' + this.speed + ' км в час');
+        }
+        this.changeDistance = function(distance){
+            this.distance = distance;
+            return this.getTimeWay();
+        }
+    }
 
-		if(vehicle == 'Машина'){
-			console.log(vehicle);
-		}else if(vehicle == 'Самолет'){
-			console.log(vehicle);
-		}else if(vehicle == 'Корабль'){
-			console.log(vehicle);
-		}
+	return window.start = function(id, vehicle){
 
-		var distance = prompt('Введите расстояние в км до цели');
-		var speed = prompt('Введите среднюю скорость для ' + vehicle + '.');
-		var car = new Car(distance, speed);
-		alert(car.getTimeWay());
-		var changeSpeedCar = confirm('Хотите изменить скорость и пересчитать время?');
-		if(changeSpeedCar){
-			var speed = prompt('Введите среднюю скорость для ' + vehicle + '.');
-			alert(car.changeSpeed(speed));
-		}
+		var distance = +prompt('Введите расстояние в км до цели');
+        switch (id){
+            case 'car':
+                var speed = +prompt( vehicle + '. Введите среднюю скорость.');
+                var car = new Car(distance, speed);
+                alert(car.getTimeWay());
+                if(distance != false){ // Если расстояние задано
+                    var changeSpeedCar = confirm('Хотите изменить скорость и пересчитать время?');
+                    if(changeSpeedCar){
+                        var speed = +prompt(vehicle + '. Укажите новую среднюю скорость. Расстояние = ' + distance + ' км');
+                        alert(car.changeSpeed(speed));
+                    }
+                }
+                break;
+            case 'plane':
+                var plane = new Plane(distance);
+                alert(plane.getTimeWay());
+                break;
+            case 'ship':
+                var ship = new Ship(distance, speed);
+                ship.getSpeed();
+                alert(ship.getTimeWay());
+                if(distance != false){ // Если расстояние задано
+                    var changeDistance = confirm('Хотите изменить расстояние и пересчитать время?');
+                    if(changeDistance){
+                        var distance = +prompt(vehicle + '. Укажите новое расстояние.');
+                        alert(ship.changeDistance(distance));
+                    }
+                }
+                break;
+        }
+
 	}
-
 
 })();
