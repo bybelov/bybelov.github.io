532a25434f1da407485c3898662394cd1235e8a6
Igor <bybelov@gmail.com>
2017-04-21 23:04:07 +0300

    update

--- skillbox/javascript/10/script.js
+++ skillbox/javascript/10/script.js
@@ -1,15 +1,29 @@
 (function (){
 
     var contentArea = document.querySelector('.js-content');
+    var buttonBlock = document.querySelector('.js-btn-block');
     var buttonEdit = document.querySelector('.js-btn-edit');
     var buttonSave = document.querySelector('.js-btn-save');
     var buttonCancel = document.querySelector('.js-btn-cancel');
-    var story = {};
+    var story = [];
 
     // Если в локальном хранилище есть запись, то загружаем текст из неё на страницу
     if(localStorage.getItem('textStorage') != null){
         var getLastTextFromHistory = JSON.parse(localStorage.getItem("textStorage"));
         contentArea.innerHTML = getLastTextFromHistory;
+
+
+         var optionArray = [];
+         for(i = 0; i <= getLastTextFromHistory.length; i++){
+             optionArray[i] = getLastTextFromHistory
+         }
+
+
+
+         var select = document.createElement('select');
+         var option = document.createElement('option')
+         buttonBlock.appendChild(select);
+         select.appendChild(option);
     }
 
     // Слушаем события клик на кнопку Редактировать
@@ -24,6 +38,7 @@
 
         }
     });
+
     // Слушаем события клик на кнопку Сохранить
     buttonSave.addEventListener('click', function(e){
         e.preventDefault();
@@ -37,9 +52,15 @@
             //contentAreaText = contentArea.innerHTML;
             //localStorage.setItem('textStorage', contentAreaText);
 
-            story[getDate()] =  contentArea.innerHTML;
+
+            story.push({
+                date : getDate(),
+                content :  contentArea.innerHTML
+            });
+
             localStorage.setItem("textStorage", JSON.stringify(story));
-            console.log(getDate());
+
+            console.log(story);
 
         }
     });
@@ -60,8 +81,8 @@
 
 
     function getDate(){
-        var curDate = new Date();
-        return curDate.toLocaleString();
+        return Date.now();
     }
+
 
 })();
