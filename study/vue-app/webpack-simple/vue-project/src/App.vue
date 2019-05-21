14fd9b2926f2d91c2d6394e218e436b3dfbd090b
Igor <bybelov@yandex.ru>
2019-05-22 00:06:03 +0300

    study Vue

---
+++ study/vue-app/webpack-simple/vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Parent: {{ carName }}</h1>
+    <app-car
+      v-bind:carName="carName"
+      v-bind:carYear="carYear"
+    ></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './Car'
+
+export default {
+
+  data () {
+    return {
+      carName: 'Ford from parent',
+      carYear: 2015
+    }
+  },
+  components:{
+    appCar: Car
+  }
+}
+</script>
---
+++ study/vue-app/webpack-simple/vue-project/src/Car.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="car">
+    <h3>Name: {{ carName }}</h3>
+    <p>Year: {{ carYear }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'carName',
+    'carYear'
+  ]
+}
+</script>
+<style>
+  .car{
+    border: 1px solid tomato;
+    padding: 8px 16px;
+  }
+  .car h3{
+    margin-bottom: 8px;
+  }
+  .car p{
+
+  }
+</style>
