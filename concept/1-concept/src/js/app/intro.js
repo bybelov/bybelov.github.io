17c6f198429ba8ed6033f05a43bc156e4ab6632c
Igor <bybelov@yandex.ru>
2019-06-04 00:29:54 +0300

    update

--- concept/1-concept/src/js/app/intro.js
+++ concept/1-concept/src/js/app/intro.js
@@ -3,13 +3,14 @@ import Swiper from 'swiper';
 export class Intro {
   constructor(selector) {
     this.$el = document.querySelector(selector);
-    console.log(this.$el);
+    // console.log(this.$el);
     Slider(this.$el);
   }
 
 }
 
 function Slider(selector) {
+
   new Swiper(selector, {
     // Optional parameters
     loop: false,
@@ -17,6 +18,23 @@ function Slider(selector) {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      type: 'custom',
+      renderCustom: function(swiper, current, total) {
+        
+        return `
+          <span class="swiper-pagination-current">
+            <span class="js-swiper-pagination-current">${current}</span>
+            <span class="js-swiper-pagination-next">${current}</span>
+          </span> 
+          <span class="swiper-pagination-separator">—</span>
+          <span class="swiper-pagination-total">${total}</span>`;
+      },
+      // type: 'fraction',
+      // renderFraction: function(currentClass, totalClass) {
+      //   return '<span class="' + currentClass + '"></span>' +
+      //           '<span class="swiper-pagination-separator">—</span>' +
+      //           '<span class="' + totalClass + '"></span>';
+      // }
     },
 
     // Navigation arrows
