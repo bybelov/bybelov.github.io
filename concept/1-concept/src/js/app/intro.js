38942cf3e57c12489f394d300b0347d0946a861f
Igor <bybelov@yandex.ru>
2019-06-05 00:50:30 +0300

    Swiper add pagination flip animation

--- concept/1-concept/src/js/app/intro.js
+++ concept/1-concept/src/js/app/intro.js
@@ -14,27 +14,38 @@ function Slider(selector) {
   new Swiper(selector, {
     // Optional parameters
     loop: false,
+    roundLengths: true,
     effect: 'fade',
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      type: 'custom',
-      renderCustom: function(swiper, current, total) {
-        
-        return `
-          <span class="swiper-pagination-current">
-            <span class="js-swiper-pagination-current">${current}</span>
-            <span class="js-swiper-pagination-next">${current}</span>
-          </span> 
-          <span class="swiper-pagination-separator">—</span>
-          <span class="swiper-pagination-total">${total}</span>`;
+      type: 'custom'
+    },
+
+    on: {
+      init: function() {
+
+        let current = this.$el[0].querySelector('.js-swiper-pagination-current');
+        let total = this.$el[0].querySelector('.swiper-pagination-total');
+        total.innerHTML = this.slides.length;
+        current.innerHTML = this.activeIndex + 1;
+
       },
-      // type: 'fraction',
-      // renderFraction: function(currentClass, totalClass) {
-      //   return '<span class="' + currentClass + '"></span>' +
-      //           '<span class="swiper-pagination-separator">—</span>' +
-      //           '<span class="' + totalClass + '"></span>';
-      // }
+      slideChange: function() {
+
+        let elements = this.pagination.$el[0],
+          index = this.activeIndex + 1,
+          currentWrapper,
+          current,
+          currentNext;
+
+        currentWrapper = elements.querySelector('.swiper-pagination-current__inner');
+        current = elements.querySelector('.js-swiper-pagination-current');
+        currentNext = elements.querySelector('.js-swiper-pagination-next');
+
+        updateNextPagination(index, currentWrapper, current, currentNext);
+
+      },
     },
 
     // Navigation arrows
@@ -43,9 +54,25 @@ function Slider(selector) {
       prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    a11y: {
+      enabled: true,
+      prevSlideMessage: 'Назад',
+      nextSlideMessage: 'Вперед'
+    }
   });
+
+  function updateNextPagination(value, wrapper, current, next) {
+
+    let timeout;
+    next.innerHTML = value;
+    wrapper.classList.add('is-changing');
+
+    window.clearTimeout(timeout);
+    timeout = window.setTimeout(function() {
+      current.innerHTML = value;
+      wrapper.classList.remove('is-changing');
+    }, 290);
+
+  }
+
 }
